127647bcdd11c2d56f99a19f67c638fa28f2f580
Fabien JUIF <fabien.juif@gmail.com>
2020-03-17 20:11:34 +0100

    :memo: adding @storybook/addon-storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../stories/**/*.stories.{js,jsx,ts,tsx}'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-storysource',
+  ],
 }
